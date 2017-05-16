260b1646968c7fd1f8536a9749e05217b7949939
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2017-05-16 23:14:34 +0900

    Initial Commit

---
+++ DevApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevApp
+//
+//  Created by 菊地弘祐 on 2017/05/16.
+//  Copyright © 2017 kou2kkkt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
